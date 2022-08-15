5a1aab798e94f308ebc340ec0a904d8c3b5516bf
darkokoa <mcdartz@gmail.com>
2022-08-15 21:03:00 +0800

    Add some configs about publishing to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,16 @@
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+
 plugins {
     kotlin("multiplatform") version "1.7.10"
+
+    id("maven-publish")
+    id("signing")
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 }
 
-//group = "dev.darkokoa"
-//version = "1.0-SNAPSHOT"
-
-plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
-    configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+plugins.withType<NodeJsRootPlugin> {
+    configure<NodeJsRootExtension> {
         nodeVersion = "16.13.1"
     }
 }
@@ -47,14 +51,14 @@ kotlin {
     }
 
     ios()
-//    watchos()
-//    tvos()
-//
-//    mingwX64()
+    watchos()
+    tvos()
+
+    mingwX64()
     macosX64()
     macosArm64()
-//    linuxX64()
-//    linuxArm64()
+    linuxX64()
+    linuxArm64()
 
     sourceSets {
         val commonTest by getting {
@@ -67,6 +71,77 @@ kotlin {
             dependencies {
                 implementation(npm("karma-detect-browsers", "^2.0"))
             }
+        }
+    }
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+publishing {
+    // Configure all publications.
+    publications.withType<MavenPublication> {
+        // Publish docs with each artifact.
+        artifact(javadocJar)
+
+        // Provide information requited by Maven Central.
+        pom {
+            name.set(rootProject.name)
+            description.set(findProperty("pomDescription") as String)
+            url.set(findProperty("pomUrl") as String)
+
+            licenses {
+                license {
+                    name.set(findProperty("pomLicenseName") as String)
+                    url.set(findProperty("pomLicenseUrl") as String)
+                }
+            }
+
+            scm {
+                url.set(findProperty("pomScmUrl") as String)
+                connection.set(findProperty("pomScmConnection") as String)
+                developerConnection.set(findProperty("pomScmDeveloperConnection") as String)
+            }
+
+            developers {
+                developer {
+                    id.set(findProperty("pomDeveloperId") as String)
+                    name.set(findProperty("pomDeveloperName") as String)
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signFileContents = File("${rootProject.projectDir}/secret/sign").readLines()
+
+    val signingKeyId = signFileContents.getOrNull(0)
+    val signingPassword = signFileContents.getOrNull(1)
+    val signingKeyBase64 = signFileContents.getOrNull(2)
+
+    if (signingKeyId != null && signingPassword != null) {
+        useInMemoryPgpKeys(signingKeyId, signingKeyBase64, signingPassword)
+    }
+
+    sign(publishing.publications)
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            val ossrhContents = File("${rootProject.projectDir}/secret/ossrh").readLines()
+            val ossrhUsername = ossrhContents.getOrNull(0)
+            val ossrhPassword = ossrhContents.getOrNull(1)
+            val ossrhStagingProfileId = ossrhContents.getOrNull(2)
+
+            username.set(ossrhUsername)
+            password.set(ossrhPassword)
+            stagingProfileId.set(ossrhStagingProfileId)
         }
     }
 }
